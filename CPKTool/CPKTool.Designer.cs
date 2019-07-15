270d3427a1b00f40710f86a173f5864879749375
Fowindy <47002339@qq.com>
2019-07-15 10:52:29 +0800

    添加项目文件。

---
+++ CPKTool/CPKTool.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace CPKTool
+{
+    partial class CPKTool
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numModifeTime = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numCreatTime = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numCPK = new System.Windows.Forms.NumericUpDown();
+            this.tPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lPath = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numModifeTime)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCreatTime)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCPK)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numModifeTime);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.numCreatTime);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.numCPK);
+            this.groupBox1.Controls.Add(this.tPath);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lPath);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(593, 235);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "配置";
+            // 
+            // numModifeTime
+            // 
+            this.numModifeTime.Location = new System.Drawing.Point(169, 165);
+            this.numModifeTime.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numModifeTime.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numModifeTime.Name = "numModifeTime";
+            this.numModifeTime.Size = new System.Drawing.Size(120, 33);
+            this.numModifeTime.TabIndex = 9;
+            this.numModifeTime.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-2, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "修改修改时间内:";
+            // 
+            // numCreatTime
+            // 
+            this.numCreatTime.Location = new System.Drawing.Point(169, 123);
+            this.numCreatTime.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numCreatTime.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numCreatTime.Name = "numCreatTime";
+            this.numCreatTime.Size = new System.Drawing.Size(120, 33);
+            this.numCreatTime.TabIndex = 7;
+            this.numCreatTime.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-2, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "监控创建时间内:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(348, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 39);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "开始监控";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numCPK
+            // 
+            this.numCPK.DecimalPlaces = 1;
+            this.numCPK.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numCPK.Location = new System.Drawing.Point(169, 73);
+            this.numCPK.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numCPK.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numCPK.Name = "numCPK";
+            this.numCPK.Size = new System.Drawing.Size(120, 33);
+            this.numCPK.TabIndex = 4;
+            this.numCPK.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            // 
+            // tPath
+            // 
+            this.tPath.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tPath.Location = new System.Drawing.Point(169, 24);
+            this.tPath.Name = "tPath";
+            this.tPath.Size = new System.Drawing.Size(267, 33);
+            this.tPath.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(56, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CPK期望:";
+            // 
+            // lPath
+            // 
+            this.lPath.AutoSize = true;
+            this.lPath.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lPath.Location = new System.Drawing.Point(55, 32);
+            this.lPath.Name = "lPath";
+            this.lPath.Size = new System.Drawing.Size(93, 25);
+            this.lPath.TabIndex = 0;
+            this.lPath.Text = "文件路径:";
+            // 
+            // CPKTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(498, 235);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "CPKTool";
+            this.Text = "CPK助手";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numModifeTime)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCreatTime)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCPK)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tPath;
+        private System.Windows.Forms.NumericUpDown numCPK;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numCreatTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numModifeTime;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CPKTool/CPKTool.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CPKTool
+{
+    public partial class CPKTool : Form
+    {
+        public CPKTool()
+        {
+            InitializeComponent();
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += Timer1_Tick;
+        }
+
+        private string Path = string.Empty;
+        private double cpkDef = 0.5;
+        private TimeSpan timeSpan1 = new TimeSpan(0, 30, 0);
+        private TimeSpan timeSpan2 = new TimeSpan(0, 5, 0);
+
+        /// <summary>
+        /// 存储修改过的文件
+        /// </summary>
+        private List<string> ChangedFileName = new List<string>();
+
+        /// <summary>
+        /// 定时监控 1s 一次
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                DirectoryInfo root = new DirectoryInfo(this.Path);
+                FileInfo[] files = root.GetFiles();
+                foreach(FileInfo fileInfo in files)
+                {
+                    if (DateTime.Now - fileInfo.CreationTime < timeSpan1) // 大于 时间间隔以上不监控
+                    {
+                        if (DateTime.Now - fileInfo.LastWriteTime > timeSpan2) // 大于 时间间隔以上才修改
+                        {
+                            if (!this.ChangedFileName.Contains(fileInfo.FullName)) // 没有修改过
+                            {
+                                this.ChangeCPK(fileInfo.FullName);
+                                this.ChangedFileName.Add(fileInfo.FullName);
+                            }
+                        }
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Path = this.tPath.Text;
+            this.timeSpan1 = new TimeSpan(0, (int)this.numCreatTime.Value, 0);
+            this.timeSpan2 = new TimeSpan(0, (int)this.numModifeTime.Value, 0);
+            this.cpkDef = (double)this.numCPK.Value;
+            this.Visible = false;
+            this.timer1.Start();
+        }
+
+        /// <summary>
+        /// 获得随机数
+        /// </summary>
+        /// <param name="seed">标准值</param>
+        /// <param name="mid">震荡幅度</param>
+        /// <returns></returns>
+        private Tuple<double, double> GetRandom(double seed, double mid)
+        {
+            Random rd = new Random();
+            var result = (rd.Next((int)(mid * 2000)) - mid * 1000) * this.cpkDef / 1000.0; // 扩大2000倍随机, 0.6 CPK >1.33 振幅就必须要小于60%
+            return new Tuple<double, double>(seed + result, result);
+        }
+
+        private void ChangeCPK(string path)
+        {
+            //string path = @"C:\Users\lichen\Desktop\0983内轨.txt";
+            string[] content = File.ReadAllLines(path);
+
+            for (int i = 0; i < content.Length; ++i)
+            {
+                if (!(content[i].Contains("BEGIN") || content[i].Contains("END")))
+                {
+                    string[] lineArg = content[i].Split('\t');
+                    if (lineArg.Length >= 6)
+                    {
+                        double real = double.Parse(lineArg[1]);
+                        double need = double.Parse(lineArg[2]);
+                        double mid = double.Parse(lineArg[3]);
+                        if (Math.Abs(real - need) >= (mid * this.cpkDef))
+                        {
+                            var tuple = GetRandom(need, mid);
+                            lineArg[1] = tuple.Item1.ToString("f4");
+                            lineArg[5] = tuple.Item2.ToString("f4");
+                            content[i] = $"{lineArg[0]}\t{lineArg[1]}\t{lineArg[2]}\t{lineArg[3]}\t{lineArg[4]}\t{lineArg[5]}\t";
+                        }
+                    }
+                }
+            }
+
+            File.WriteAllLines(path, content);
+        }
+    }
+}
